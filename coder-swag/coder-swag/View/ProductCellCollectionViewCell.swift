36d520314520ce596dbd0ee4b823307e86064567
Jasmine <jasmine.chan89@gmail.com>
2018-04-23 22:36:23 -0400

    grid creation

---
+++ coder-swag/coder-swag/View/ProductCellCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCellCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by JasmineChan on 2018-04-23.
+//  Copyright © 2018 chanizzle. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
